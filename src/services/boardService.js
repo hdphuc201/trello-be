280c88eb4c814dc762bd4e55dc14ed968f80cc88
Huỳnh Duy Phúc <83865270+HuynhDuyPhuc201@users.noreply.github.com>
2024-02-19 11:30:44 +0700

    api_create_board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -4,6 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
+import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
 
@@ -16,13 +17,16 @@ const createNew = async (reqBody) => {
     }
 
     // gọi cho tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ...
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // lấy bản ghi board sau khi gọi ( tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
 
     // Làm thêm các xử lý logic khác vs các collection khác tùy đặc thù dự án...
     // Bắn Email, notification về cho admin khi có 1 cái board mới được tạo...
 
     // trả kết quả về trong Service, luôn phải có return
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw new Error(error)
   }
