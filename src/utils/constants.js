acf1a9cb024c9ed12eae718edd75426b33d47a52
Huỳnh Duy Phúc <83865270+HuynhDuyPhuc201@users.noreply.github.com>
2024-03-09 19:15:30 +0700

    update Whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,6 +7,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:3000'
   // Không cần local nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev)
+  'https://trello-web-rho.vercel.app/'
   //...vv, ví dụ sau này sẽ deloy lên domain chính thức...vv
 ]
 
