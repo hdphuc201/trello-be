fc92ee29884cc6ad1c169b2e998e6cfc4578f0c2
unknown <hdphuc201@gmail.com>
2025-04-29 07:54:06 +0700

    feat: update server

--- src/server.js
+++ src/server.js
@@ -14,46 +14,33 @@ import { APIs_V1 } from './routes/v1'
 import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 import { corsOptions } from './config/cors'
 
+const app = express()
+
+app.use(cors(corsOptions))
+
+// Enable req.body json data
+app.use(express.json())
+
+// use APIs V1
+app.use('/v1', APIs_V1)
+
+// Middleware xử lý lỗi tập trung
+app.use(errorHandlingMiddleware)
+
+// Start server
 const START_SERVER = () => {
-  const app = express()
+  const port = env.BUILD_MODE === 'production' ? process.env.PORT : env.LOCAL_DEV_APP_PORT
+  const host = env.BUILD_MODE === 'production' ? undefined : env.LOCAL_DEV_APP_HOST
 
-  app.use(cors(corsOptions))
-
-  // Enable req.body json data
-  app.use(express.json())
-
-  // use APIs V1
-  app.use('/v1', APIs_V1)
-
-  // Middleware xử lý lỗi tập trung
-  app.use(errorHandlingMiddleware)
-
-  // Môi trường production (cụ thể hiện tại là đang support render.com)
-  if (env.BUILD_MODE === 'production') {
-    app.listen(process.env.PORT, () => {
-      console.log(
-        `3. Production: Hello ${env.AUTHOR}, Back-end server is running successully at Port: ${process.env.PORT}`
-      )
-    })
-  } else {
-    // Môi trường local dev
-    app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
-      console.log(
-        `3. Local DEV: Hello ${env.AUTHOR}, Back-end server is running successully at Host: ${env.LOCAL_DEV_APP_HOST} and Port: ${env.LOCAL_DEV_APP_PORT}`
-      )
-    })
-  }
-
-  // commnet video 48 có giải thích: nên sử dụng linux-ubuntun, sd window dễ gặp tình trạng lỗi
-  // Ctrl + C lúc được lúc không
-  exitHook(() => {
-    console.log('4. Server is shutting down...')
-    CLOSE_DB()
-    console.log('5. Disconnected from MongoDB')
+  app.listen(port, host, () => {
+    console.log(`${env.BUILD_MODE === 'production' ? 'Production' : 'Dev'}: Server running at ${host ?? ''}:${port}`)
   })
 }
-
-// Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên
+exitHook(() => {
+  console.log('4. Server is shutting down...')
+  CLOSE_DB()
+  console.log('5. Disconnected from MongoDB')
+})
 
 // Imediately-invoked / Anonymous Async Functions (IIFE)
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
