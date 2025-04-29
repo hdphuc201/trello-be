a13dfcad8a586948d5f24196d01bd81fcc1e1346
unknown <hdphuc201@gmail.com>
2025-04-29 09:20:33 +0700

    feat: update whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,7 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
 // Những domain được phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  // 'http://localhost:3000'
-  // Không cần local nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev)
-  'https://trello-web-rho.vercel.app'
-  //...vv, ví dụ sau này sẽ deloy lên domain chính thức...vv
+  'http://localhost:3000',
+  'https://hdphuc-trello.vercel.app'
 ]
 
 export const BOARD_TYPES = {
