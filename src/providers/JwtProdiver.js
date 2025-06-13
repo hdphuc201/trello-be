0e5c4dee65d85093128d29ab6ae0099475f12e1b
Phúc Huỳnh Duy <hdphuc201@gmail.com>
2025-06-13 16:37:49 +0700

    fix: bug login gg

--- src/providers/JwtProdiver.js
+++ src/providers/JwtProdiver.js
@@ -4,11 +4,11 @@ import { v4 as uuidv4 } from 'uuid'
 import { env } from '~/config/environment'
 
 const generateAccessToken = (user) => {
-  return jwt.sign({ _id: user?._id, email: user?.email, jti: uuidv4() }, env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+  return jwt.sign({ _id: user?._id, email: user?.email, jti: uuidv4() }, env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
 }
 
 const generateRefreshToken = (user) => {
-  return jwt.sign({ _id: user?._id, email: user?.email, jti: uuidv4() }, env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+  return jwt.sign({ _id: user?._id, email: user?.email, jti: uuidv4() }, env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' })
 }
 
 const verifyToken = (token, secretSignature) => {
